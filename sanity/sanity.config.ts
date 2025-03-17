c2785c8a6228fc845f7f233662f94ee55cdb2411
kalpsenghani <kalpsenghani0601@gmail.com>
2025-03-17 01:20:18 -0400

    Solved error

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   name: 'default',
   title: 'Your Portfolio',
 
-  projectId: process.env.woz7zt3l, // Ensure this matches your Sanity project ID
-  dataset: process.env.production, // Make sure the dataset is correctly set in your .env
+  projectId: process.env.woz7zt3l || 'woz7zt3l', // Provide a default value
+  dataset: process.env.production || 'production', // Provide a default value
 
   plugins: [deskTool(), visionTool()],
 })
